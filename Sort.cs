d36d32d2eb7d9723a43d7b77be73b47461e6c7bb
Pravnat <91091738+Pravnat@users.noreply.github.com>
2021-11-05 10:47:32 +0530

    day10

---
+++ Sort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobDetails
+{
+    class Sort:IComparer<Job>
+    {
+        public int Compare(Job x, Job y)
+        {
+            return x.Sal.CompareTo(y.Sal);
+        }
+    }
+}
